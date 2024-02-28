3cc6d71f2fae894e99adfe5bf8f53e778b5e5856
yuik23 <yui_karamatsu@yahoo.co.jp>
2024-02-29 08:10:48 +0900

    modification of function to add item

--- app/src/main/java/com/websarva/wings/android/applicationforvisualizingbudgets/CreateListFragment.java
+++ app/src/main/java/com/websarva/wings/android/applicationforvisualizingbudgets/CreateListFragment.java
@@ -1,33 +1,27 @@
 package com.websarva.wings.android.applicationforvisualizingbudgets;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import android.view.LayoutInflater;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-
-import androidx.core.util.Pair;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.widget.EditText;
 import android.widget.Filter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.util.Pair;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.material.datepicker.MaterialDatePicker;
 
@@ -43,12 +37,27 @@ public class CreateListFragment extends Fragment{
         super(R.layout.fragment_create_list);
     }
 
+    //期間入力時に選択された期間を格納
     private TextView selectedDate;
-    private ItemArrayAdapter adapter;
-    public static Map<String, String> data;
-    public static List<Map<String, String>> dataList;
-    public static ListView listView;
-    public static ItemListViewAdapter listAdapter;
+
+    //予算項目入力時の候補表示用アダプター
+    private ItemArrayAdapter itemAdapter;
+
+    //ListView表示用
+    public SimpleAdapter itemListAdapter;
+    public List<Map<String, String>> itemList;
+    public Map<String, String> itemData;
+    public ImageButton ibDeleteRowV;
+
+
+    //入力項目取得用
+    public AutoCompleteTextView actvItemV;
+    public String inputItem;
+    public EditText etItemBudgetV;
+    public Integer inputItemBudget;
+
+
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -57,8 +66,13 @@ public class CreateListFragment extends Fragment{
         //OnDateSetへ受け渡しのためビューをあらかじめ取得
         selectedDate=view.findViewById(R.id.tvPeriodDisplay);
 
-        //ListView
-        dataList = new ArrayList<Map<String, String>>();
+        //プラスボタンリスナへ受け渡しのためビューをあらかじめ取得
+        actvItemV=view.findViewById(R.id.actvItem);
+        etItemBudgetV=view.findViewById(R.id.etItemBudget);
+
+        //リストビューの行削除のため削除ボタンのビューをあらかじめ取得
+        ibDeleteRowV=view.findViewById(R.id.ibDeleteRow);
+
 
         //Toolbarを取得
         Toolbar toolbar = view.findViewById(R.id.toolbar);
@@ -75,30 +89,56 @@ public class CreateListFragment extends Fragment{
         // Fragment#onOptionsItemSelectedを有効にする。
         setHasOptionsMenu(true);
 
+
         //期間入力ボタンにリスナ設定
         ImageButton ibPeriod =view.findViewById(R.id.ibPeriodInput);
         ibPeriod.setOnClickListener(new BtPeriodClickListener());
+
 
         //クリアボタンにリスナ設定
         ImageButton ibClear =view.findViewById(R.id.ibClear);
         ibClear.setOnClickListener(new btClearClickListener());
 
+
         //予算項目入力時に候補を表示する
         String[] itemArray = {"食費", "日用品費", "医療費", "子ども費", "被服費", "美容費", "交際費", "娯楽費", "雑費", "特別費"};
-        adapter = new ItemArrayAdapter (requireContext(), android.R.layout.simple_dropdown_item_1line, itemArray);
-        AutoCompleteTextView actvItem = view.findViewById(R.id.actvItem);
-        actvItem .setAdapter(adapter);
+        itemAdapter = new ItemArrayAdapter (requireContext(), android.R.layout.simple_dropdown_item_1line, itemArray);
+        actvItemV .setAdapter(itemAdapter);
         //未入力(フォーカス、タップ)で候補を表示
-        actvItem.setThreshold(1);
-        actvItem.setOnFocusChangeListener((v, hasFocus) -> {
+        actvItemV.setThreshold(1);
+        actvItemV.setOnFocusChangeListener((v, hasFocus) -> {
             if (hasFocus) {
-                actvItem.showDropDown();
+                actvItemV.showDropDown();
             }
         });
-        actvItem.setOnClickListener(v -> actvItem.showDropDown());
+        actvItemV.setOnClickListener(v -> actvItemV.showDropDown());
+        //enterキーが押されたときソフトキーボードを隠す
+        actvItemV.setOnEditorActionListener(new enterClickListener());
+        etItemBudgetV.setOnEditorActionListener(new enterClickListener());
 
 
-        // ListViewに表示するためのDATAを作成する
+        //ListViewオブジェクトを取得
+        ListView itemListView=view.findViewById(R.id.lvBudgetItems);
+        //ListViewに表示するリストデータ用Listオブジェクトを作成
+        itemList = new ArrayList<Map<String, String>>();
+        //項目データを格納するMapオブジェクトを用意
+        itemData = new HashMap<String,String>();
+        //SimpleAdapter第4引数from用データの用意
+        String[] from={"item","itemBudget"};
+        //SimpleAdapter第5引数to用データの用意
+        int[] to={R.id.tvItemRow,R.id.tvItemBudgetRow};
+        //アダプタオブジェクトを生成
+        itemListAdapter=new SimpleAdapter(requireContext(),itemList,R.layout.row_item_list,from,to);
+        //リストビューにアダプタオブジェクトを設定
+        itemListView.setAdapter(itemListAdapter);
+
+
+        //プラスボタンにリスナ設定
+        ImageButton ibAddItem=view.findViewById(R.id.ibAddItem);
+        ibAddItem.setOnClickListener(new ibAddItemClickListener());
+
+
+       /* // ListViewに表示するためのDATAを作成する
         int MAXDATA = 10;
         for (int i = 0; i < MAXDATA; i++) {
             data = new HashMap<String, String>();
@@ -119,7 +159,7 @@ public class CreateListFragment extends Fragment{
         // ListViewにアダプターをSETする
         listView = (ListView) view.findViewById(R.id.lvBudgetItems);
         listView.setAdapter(listAdapter);
-        listView.setTextFilterEnabled(false);
+        listView.setTextFilterEnabled(false);*/
     }
 
     //MenuItemが押されると呼ばれるメソッド
@@ -196,9 +236,9 @@ public class CreateListFragment extends Fragment{
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
             if (results.count > 0) {
-                adapter.notifyDataSetChanged();
+                itemAdapter.notifyDataSetChanged();
             } else {
-                adapter.notifyDataSetInvalidated();
+                itemAdapter.notifyDataSetInvalidated();
             }
         }
     }
@@ -215,4 +255,58 @@ public class CreateListFragment extends Fragment{
         }
     }
 
+    //項目入力欄のリスナ。enterキー入力時にソフトウェアキーボードを隠す処理
+    private class enterClickListener implements TextView.OnEditorActionListener {
+
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+            if (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                if (event.getAction() == KeyEvent.ACTION_UP) {
+                    // ソフトウェアキーボードを隠す
+                    InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+                    return true;
+            }
+                return false;
+        }
+    }
+
+
+    //プラスボタンのリスナ
+    private class ibAddItemClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            //入力がある場合、項目入力欄の値を取得、クリア
+            if((actvItemV.length()!=0)&&(etItemBudgetV.length()!=0)) {
+                inputItem = actvItemV.getText().toString();
+                actvItemV.setText("");
+                inputItemBudget = Integer.parseInt(etItemBudgetV.getText().toString());
+                etItemBudgetV.setText("");
+
+                //ListViewに取得した値を格納
+                itemData = new HashMap<>();
+                itemData.put("item", inputItem);
+                itemData.put("itemBudget", inputItemBudget + "円");
+                itemList.add(itemData);
+                itemListAdapter.notifyDataSetChanged();
+                //削除ボタンにリスナ設定
+               /* //リストビューの行削除のため削除ボタンのビューをあらかじめ取得
+                ibDeleteRowV=view.findViewById(R.id.ibDeleteRow);
+                ibDeleteRowV.setOnClickListener(new ibDeleteItemClickListener());
+                ibDeleteRowV.setTag(itemList.size()-1);*/
+            }
+        }
+    }
+
+    //削除ボタンのリスナ
+    private class ibDeleteItemClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            itemList.remove(ibDeleteRowV.getTag());
+
+
+        }
+
+    }
 }
